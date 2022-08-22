8049f17a0b7e1751387cda56637ebb5f0009d20a
Mads Skovgaard <madsskovgaard@hotmail.com>
2022-08-22 18:40:57 +0200

    Fixes deprecated GameDamage with another damage type constructor.

--- src/main/java/examplemod/examples/ExampleProjectileWeapon.java
+++ src/main/java/examplemod/examples/ExampleProjectileWeapon.java
@@ -4,6 +4,7 @@ import necesse.engine.Screen;
 import necesse.engine.localization.Localization;
 import necesse.engine.network.PacketReader;
 import necesse.engine.network.packet.PacketSpawnProjectile;
+import necesse.engine.registries.DamageTypeRegistry;
 import necesse.engine.sound.SoundEffect;
 import necesse.engine.util.GameRandom;
 import necesse.entity.mobs.AttackAnimMob;
@@ -27,7 +28,7 @@ public class ExampleProjectileWeapon extends ProjectileToolItem {
         super(400);
         rarity = Rarity.RARE;
         animSpeed = 300; // 300 ms attack time
-        attackDmg = new GameDamage(GameDamage.DamageType.MAGIC, 20); // 20 magic damage
+        attackDmg = new GameDamage(DamageTypeRegistry.MAGIC, 20); // 20 magic damage
         velocity = 100; // Velocity of projectiles
         knockback = 50; // Knockback of projectiles
         attackRange = 1500; // Range of the projectile
