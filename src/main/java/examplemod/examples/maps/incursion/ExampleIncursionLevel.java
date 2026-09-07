ab29a0697cd2467c42b1d0c4ab99dfbb6816d1a8
Mads Skovgaard <madsskovgaard@hotmail.com>
2026-09-07 11:31:55 +0200

    Adds a few comment to ExampleIncursionLevel.

--- src/main/java/examplemod/examples/maps/incursion/ExampleIncursionLevel.java
+++ src/main/java/examplemod/examples/maps/incursion/ExampleIncursionLevel.java
@@ -31,8 +31,8 @@ public class ExampleIncursionLevel extends IncursionLevel {
      */
     public ExampleIncursionLevel(LevelIdentifier identifier, int width, int height, WorldEntity worldEntity) {
         super(identifier, width, height, worldEntity);
-        this.baseBiome = ExampleMod.EXAMPLE_BIOME;
-        this.isCave = true;
+        baseBiome = ExampleMod.EXAMPLE_BIOME;
+        isCave = true;
     }
 
     /**
@@ -41,24 +41,41 @@ public class ExampleIncursionLevel extends IncursionLevel {
      */
     public ExampleIncursionLevel(LevelIdentifier identifier, BiomeMissionIncursionData incursionData, WorldEntity worldEntity, AltarData altarData) {
         super(identifier, 150, 150, incursionData, worldEntity);
-        this.baseBiome = ExampleMod.EXAMPLE_BIOME;
-        this.isCave = true;
+        baseBiome = ExampleMod.EXAMPLE_BIOME;
+        isCave = true;
+
+        // Generate the full level
         generateLevel(incursionData, altarData);
     }
 
     public void generateLevel(BiomeMissionIncursionData incursionData, AltarData altarData) {
+        // CaveGeneration is a helper class for stuff like generating the cave layout, placing ore veins, etc.
         CaveGeneration cg = new CaveGeneration(this, "deeprocktile", "examplebaserock");
+
+        // We set the seed for CaveGeneration to be based on our tablets seed
         cg.random.setSeed(incursionData.getUniqueID());
 
+        // During generation, the base game runs a bunch of events when generating stuff. This allows other
+        // mods to intercept and modify these events, to alter the generation if they wish. It's not a
+        // requirement to use these, but is recommended
         GameEvents.triggerEvent(
                 new GenerateCaveLayoutEvent(this, cg),
-                e -> cg.generateLevel(0.38F, 4, 3, 6)
+                e -> {
+                    // In here we actually run the generation if the event was not prevented
+                    // CaveGeneration uses Cellular Automaton to generate the caves. It's slightly different to
+                    // how we generate caves in the infinite world generation, since this time around the level is
+                    // limited and is generated at its entirety in this method
+
+                    // These values are fine-tuned, so we get a standard looking cave layout
+                    cg.generateLevel(0.38f, 4, 3, 6);
+                }
         );
 
-        // Keeps track of occupied space when trying to place presets
+        // PresetGeneration is another helper class, which helps us to place presets into the level,
+        // while keeping track of the occupied space, so we don't overlap them if we don't want to
         PresetGeneration presets = new PresetGeneration(this);
 
-        // Generate entrance (this reserves space inside presets)
+        // Generate entrance (this reserves space inside our PresetGeneration)
         int spawnSize = 32;
         boolean hasBiggerArenaPerk = altarData.hasPerk(IncursionPerksRegistry.BIGGER_ARENA);
         IncursionBiome.generateEntrance(
@@ -98,7 +115,8 @@ public class ExampleIncursionLevel extends IncursionLevel {
         if (incursionData instanceof BiomeExtractionIncursionData) {
             cg.generateGuaranteedOreVeins(40, 4, 8, ObjectRegistry.getObjectID("exampleorerock"));
         }
-        // Generate upgrade shard and alchemy shard ores
+        // Generate upgrade shard and alchemy shard ores. The amount of ores placed is very deliberate,
+        // so that there is no advantage to farm this incursion than the others
         cg.generateGuaranteedOreVeins(75, 6, 12, ObjectRegistry.getObjectID("upgradesharddeeprock"));
         cg.generateGuaranteedOreVeins(75, 6, 12, ObjectRegistry.getObjectID("alchemysharddeeprock"));
 
